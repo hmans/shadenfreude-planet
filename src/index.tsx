06388bf2608a4f75f4d398db6af7619a5252a177
Hendrik Mans <hendrik@mans.de>
2022-07-10 20:00:49 +0200

    Yes.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
+const root = ReactDOMClient.createRoot(rootElement as Element);
 
 root.render(<App />);
